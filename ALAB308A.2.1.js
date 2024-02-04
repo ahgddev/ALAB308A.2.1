e481a18b24900ced82762f9cfee084e0ace7cde0
ahgddev <ahgddev@gmail.com>
2024-02-04 14:43:17 -0700

    Making sure the new Character class works.

--- ALAB308A.2.1.js
+++ ALAB308A.2.1.js
@@ -1,21 +1,21 @@
-const adventurer = {
-    name: "Robin",
-    health: 10,
-    inventory: ["sword", "potion", "artifact"],
-    companion: {
-        name: "Leo",
-        type: "Cat",
-        companion: {
-            name: "Frank",
-            type: "Flea",
-            inventory: ["small hat", "sunglasses"]
-        }
-    },
-    roll (mod = 0) {
-        const result = Math.floor(Math.random() * 20) + 1 + mod;
-        console.log(`${this.name} rolled a ${result}.`)
-    }
-}
+// const adventurer = {
+//     name: "Robin",
+//     health: 10,
+//     inventory: ["sword", "potion", "artifact"],
+//     companion: {
+//         name: "Leo",
+//         type: "Cat",
+//         companion: {
+//             name: "Frank",
+//             type: "Flea",
+//             inventory: ["small hat", "sunglasses"]
+//         }
+//     },
+//     roll (mod = 0) {
+//         const result = Math.floor(Math.random() * 20) + 1 + mod;
+//         console.log(`${this.name} rolled a ${result}.`)
+//     }
+// }
 
 function getInventory(adv){
     adv.inventory.forEach(element => {
@@ -23,5 +23,28 @@ function getInventory(adv){
     });
 }
 
-getInventory(adventurer)
-adventurer.roll() 
+// getInventory(adventurer)
+// adventurer.roll() 
+
+class Character {
+    constructor (name) {
+      this.name = name;
+      this.health = 100;
+      this.inventory = [];
+    }
+
+    roll = function (mod = 0) {
+        const result = Math.floor(Math.random() * 20) + 1 + mod;
+        console.log(`${this.name} rolled a ${result}.`)
+    }
+  }
+
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
+
+robin.roll();
