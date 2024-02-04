65c33b126830a32910d74784753cd16b405b7a3c
ahgddev <ahgddev@gmail.com>
2024-02-04 14:39:19 -0700

    Checking that the roll object works.

--- ALAB308A.2.1.js
+++ ALAB308A.2.1.js
@@ -1,7 +1,20 @@
 const adventurer = {
     name: "Robin",
     health: 10,
-    inventory: ["sword", "potion", "artifact"]
+    inventory: ["sword", "potion", "artifact"],
+    companion: {
+        name: "Leo",
+        type: "Cat",
+        companion: {
+            name: "Frank",
+            type: "Flea",
+            inventory: ["small hat", "sunglasses"]
+        }
+    },
+    roll (mod = 0) {
+        const result = Math.floor(Math.random() * 20) + 1 + mod;
+        console.log(`${this.name} rolled a ${result}.`)
+    }
 }
 
 function getInventory(adv){
@@ -11,3 +24,4 @@ function getInventory(adv){
 }
 
 getInventory(adventurer)
+adventurer.roll() 
