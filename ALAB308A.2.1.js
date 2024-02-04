aebeaba7e58dc210a91d650bfcb7ad40af842576
ahgddev <ahgddev@gmail.com>
2024-02-04 15:08:28 -0700

    Starting part 4.

--- ALAB308A.2.1.js
+++ ALAB308A.2.1.js
@@ -1,3 +1,4 @@
+//Part 1: Humble Beginnings
 // const adventurer = {
 //     name: "Robin",
 //     health: 10,
@@ -26,6 +27,7 @@ function getInventory(adv){
 // getInventory(adventurer)
 // adventurer.roll() 
 
+//Part 2: Class Fantasy
 class Character {
     constructor (name) {
       this.name = name;
@@ -39,12 +41,64 @@ class Character {
     }
   }
 
-const robin = new Character("Robin");
-robin.inventory = ["sword", "potion", "artifact"];
-robin.companion = new Character("Leo");
-robin.companion.type = "Cat";
-robin.companion.companion = new Character("Frank");
-robin.companion.companion.type = "Flea";
-robin.companion.companion.inventory = ["small hat", "sunglasses"];
+// const robin = new Character("Robin");
+// robin.inventory = ["sword", "potion", "artifact"];
+// robin.companion = new Character("Leo");
+// robin.companion.type = "Cat";
+// robin.companion.companion = new Character("Frank");
+// robin.companion.companion.type = "Flea";
+// robin.companion.companion.inventory = ["small hat", "sunglasses"];
 
-robin.roll();
+// robin.roll();
+
+//Part 3: Class Features
+class Adventurer extends Character {
+    constructor (name, role) {
+      super(name);
+      // Adventurers have specialized roles.
+      this.role = role;
+      // Every adventurer starts with a bed and 50 gold coins.
+      this.inventory.push("bedroll", "50 gold coins");
+      // Adventurers start with a bow.
+      this.inventory.push("bow");
+    }
+    //Part 4: Class Uniforms
+    static MAX_HEALTH = 100;
+    static ROLES = ["Fighter", "Healer", "Wizard", "Dragon", "Werewolf","Adventurer"];
+    // Adventurers have the ability to scout ahead of them.
+    scout () {
+      console.log(`${this.name} is scouting ahead...`);
+      super.roll();
+    }
+    // Adventurers can bargin with NPCs
+    bargin () {
+        console.log(`${this.name} is bargaining...`);
+        let bargainRoll = Math.random()*10
+        if(bargainRoll < 5){
+            console.log(`${this.name} failed to bargain!`);
+            return false
+        } else {
+            console.log(`${this.name} successfully bargained!`);
+            return true
+        }
+      } 
+  }
+
+  class Companion extends Character {
+    constructor (name, type) {
+      super(name);
+      this.type = type;
+    }
+  }
+
+  const robin = new Adventurer("Robin","Adventurer");
+  const leo = new Companion("Leo", "Cat");
+  const frank = new Companion("Frank", "Flea")
+  // robin.inventory = ["sword", "potion", "artifact"];
+  // robin.companion = new Character("Leo");
+  // robin.companion.type = "Cat";
+  // robin.companion.companion = new Character("Frank");
+  // robin.companion.companion.type = "Flea";
+  // robin.companion.companion.inventory = ["small hat", "sunglasses"];
+  
+  // robin.roll();
